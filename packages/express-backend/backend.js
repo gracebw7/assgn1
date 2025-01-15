ddf8383bcaab05e3b08d0eb4bef2e3eff3091e6f
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-01-15 13:09:00 -0800

    get by query complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,15 +5,26 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/users", (req, res) => {
-  res.send(users);
+  res.send("Hello World!!!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
 const users = {
