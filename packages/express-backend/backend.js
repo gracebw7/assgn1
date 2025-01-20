e2015980fad66678ed6ad7c62277deeae91db42d
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-01-20 15:00:01 -0800

    feat step 5 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,8 +15,11 @@ app.listen(port, () => {
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 const findUserByJob = (job) => {
-    return users["users_list"].filter((user) => user["job"] === job);
+  return users["users_list"].filter((user) => user["job"] === job);
 };
 
 app.get("/users", (req, res) => {
@@ -27,6 +30,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    res.send(result);
   }
 });
 
